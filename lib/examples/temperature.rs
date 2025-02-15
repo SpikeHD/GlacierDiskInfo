571ce6fc3fdec233562eca0acdcdb53dfe69f219
SpikeHD <25207995+SpikeHD@users.noreply.github.com>
2025-02-14 17:22:35 -0800

    feat: temperature example

---
+++ lib/examples/temperature.rs
@@ -0,0 +1,14 @@
+fn main() {
+  sudo::escalate_if_needed().expect("Failed to escalate privileges");
+
+  let disks = libglacierdisk::list_disks().unwrap();
+  let mut first = disks.first().unwrap().clone();
+  let temp = first.raw_disk().get_temperature().unwrap_or(0);
+
+  println!("{:?} mkelvin", temp);
+  println!("{:?} celsius", celsius(temp));
+}
+
+fn celsius(mkelvin: u64) -> f32 {
+  (mkelvin as f32 / 1000.) - 273.15
+}
