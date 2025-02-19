f0a7991b1a33290c8031efd665f313827f5120e5
SpikeHD <25207995+SpikeHD@users.noreply.github.com>
2025-02-18 16:06:49 -0800

    feat: dioxus_native test

--- glacierdiskinfo/src/main.rs
+++ glacierdiskinfo/src/main.rs
@@ -36,17 +36,19 @@ fn main() {
     }
   };
 
-  let window = WindowBuilder::new()
-    .with_title("GlacierDiskInfo")
-    .with_resizable(true)
-    .with_inner_size(LogicalSize::new(1500, 800))
-    .with_min_inner_size(LogicalSize::new(1500, 800));
+  // let window = WindowBuilder::new()
+  //   .with_title("GlacierDiskInfo")
+  //   .with_resizable(true)
+  //   .with_inner_size(LogicalSize::new(1500, 800))
+  //   .with_min_inner_size(LogicalSize::new(1500, 800));
 
-  let config = Config::default()
-    .with_menu(Some(menu::create_menu()))
-    .with_window(window);
+  // let config = Config::default()
+  //   .with_menu(Some(menu::create_menu()))
+  //   .with_window(window);
 
-  dioxus::LaunchBuilder::new().with_cfg(config).launch(Root);
+  //dioxus::LaunchBuilder::new().with_cfg(config).launch(Root);
+
+  dioxus_native::launch(Root);
 }
 
 #[component]
@@ -62,41 +64,41 @@ fn Root() -> Element {
   });
 
   // Submenu handler
-  dioxus::desktop::use_muda_event_handler(move |e| {
-    let id = match e.id() {
-      MenuId(s) => s,
-    }
-    .to_owned();
+  // dioxus::desktop::use_muda_event_handler(move |e| {
+  //   let id = match e.id() {
+  //     MenuId(s) => s,
+  //   }
+  //   .to_owned();
 
-    if id.starts_with("apply-") {
-      let mut config = config::load_config(App::GlacierDiskInfo).unwrap_or_default();
-      let name = id.strip_prefix("apply-").unwrap_or_default();
+  //   if id.starts_with("apply-") {
+  //     let mut config = config::load_config(App::GlacierDiskInfo).unwrap_or_default();
+  //     let name = id.strip_prefix("apply-").unwrap_or_default();
 
-      println!("Applying theme: {name}");
+  //     println!("Applying theme: {name}");
 
-      config.theme = name.to_string();
-      config::save_config(App::GlacierDiskInfo, &config).unwrap_or_default();
+  //     config.theme = name.to_string();
+  //     config::save_config(App::GlacierDiskInfo, &config).unwrap_or_default();
 
-      // Apply the CSS
-      if let Some(theme) = config.get_theme() {
-        let contents = read_theme_contents(&theme).unwrap_or_default();
-        theme_css.set(contents);
-      } else {
-        theme_css.set("".to_string());
-      }
-    } else if id == "add-theme" {
-      let theme_path = theme::theme_path(App::GlacierDiskInfo);
-      open::that_detached(theme_path).unwrap_or_default();
-    } else if id == "about" {
-      let version = env!("CARGO_PKG_VERSION");
-      let git_sha = option_env!("GIT_SHA").unwrap_or("unknown revision");
+  //     // Apply the CSS
+  //     if let Some(theme) = config.get_theme() {
+  //       let contents = read_theme_contents(&theme).unwrap_or_default();
+  //       theme_css.set(contents);
+  //     } else {
+  //       theme_css.set("".to_string());
+  //     }
+  //   } else if id == "add-theme" {
+  //     let theme_path = theme::theme_path(App::GlacierDiskInfo);
+  //     open::that_detached(theme_path).unwrap_or_default();
+  //   } else if id == "about" {
+  //     let version = env!("CARGO_PKG_VERSION");
+  //     let git_sha = option_env!("GIT_SHA").unwrap_or("unknown revision");
 
-      Message::new(format!("GlacierDiskInfo GUI v{version} ({git_sha})\n\nhttps://github.com/SpikeHD/GlacierDiskInfo\n\nCreated by SpikeHD, inspired by CrystalDiskInfo"))
-        .title("About")
-        .show()
-        .expect("Failed to show dialog");
-    }
-  });
+  //     Message::new(format!("GlacierDiskInfo GUI v{version} ({git_sha})\n\nhttps://github.com/SpikeHD/GlacierDiskInfo\n\nCreated by SpikeHD, inspired by CrystalDiskInfo"))
+  //       .title("About")
+  //       .show()
+  //       .expect("Failed to show dialog");
+  //   }
+  // });
 
   let mut selected_drive = use_signal(|| DRIVES.resolve()[0].0.clone());
 
